84d05e32af6c2ab1382bace4372c92ee0478a6e1
Rahul Saini <rahul.saini1@yatra.com>
2017-11-18 13:04:08 +0530

    First commit

---
+++ Stoic Investing/APIConnection.h
@@ -0,0 +1,13 @@
+//
+//  APIConnection.h
+//  Stoic Investing
+//
+//  Created by Rahul Saini on 17/11/17.
+//  Copyright © 2017 Stoic Investing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIConnection : NSObject
+
+@end
---
+++ Stoic Investing/APIConnection.m
@@ -0,0 +1,13 @@
+//
+//  APIConnection.m
+//  Stoic Investing
+//
+//  Created by Rahul Saini on 17/11/17.
+//  Copyright © 2017 Stoic Investing. All rights reserved.
+//
+
+#import "APIConnection.h"
+
+@implementation APIConnection
+
+@end
